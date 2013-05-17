49825f6fe6b653ebaad762334f9de0ddecf0fb64
Reza Rahman <reza_rahman@lycos.com>
2013-05-17 13:43:54 -0400

    License.

--- src/main/java/org/glassfish/jpanosqldemo/Order.java
+++ src/main/java/org/glassfish/jpanosqldemo/Order.java
@@ -1,13 +1,3 @@
-/**
- * *****************************************************************************
- * Copyright (c) 2012 Oracle. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 and Eclipse Distribution License v. 1.0 which accompanies
- * this distribution. The Eclipse Public License is available at
- * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
- * License is available at http://www.eclipse.org/org/documents/edl-v10.php.
- * ****************************************************************************
- */
 package org.glassfish.jpanosqldemo;
 
 import java.io.Serializable;
@@ -47,7 +37,7 @@ public class Order implements Serializable {
     private Address billingAddress;
     @Embedded
     private Address shippingAddress;
-    // Relationships are supported, the id is stored as a foreign key, 
+    // Relationships are supported, the id is stored as a foreign key,
     // for OneToMany a collection of ids would be stored.
     @ManyToOne
     private Customer customer;
